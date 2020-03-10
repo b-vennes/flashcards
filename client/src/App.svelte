bf275faf623d1b7f2e2cde97de2f5fd65dd216fa
Branden <b.n.vennes@gmail.com>
2020-03-10 08:31:15 -0700

    Initial commit.

---
+++ client/src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Card from './Card.svelte';
+	let name = 'world';
+</script>
+
+<h1>Flashcards App</h1>
+
+<Card word="moil" definition="to work hard; drudge."/>
---
+++ client/src/Card.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let word;
+    export let definition;
+
+    let showDefinition = false;
+
+    let toggleDefinition = () => showDefinition = !showDefinition; 
+</script>
+
+<h2>{word}</h2>
+<p>{word}</p>
+
+<button on:click={toggleDefinition}>Show Definition</button>
+
+{#if showDefinition}
+    <h3>{definition}</h3>
+{/if}
